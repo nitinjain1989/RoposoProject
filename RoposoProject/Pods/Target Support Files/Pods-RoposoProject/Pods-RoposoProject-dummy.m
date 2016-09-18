08ccfec5923850fdf71a748a9761012135894124
Nitin Jain <nitin.jain@popxo.com>
2016-09-18 21:21:43 +0530

    Add cocoa poads

---
+++ RoposoProject/Pods/Target Support Files/Pods-RoposoProject/Pods-RoposoProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RoposoProject : NSObject
+@end
+@implementation PodsDummy_Pods_RoposoProject
+@end
---
+++ RoposoProject/Pods/Target Support Files/Pods-RoposoProject/Pods-RoposoProject-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RoposoProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RoposoProjectVersionString[];
+
